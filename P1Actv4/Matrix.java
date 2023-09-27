2dedbd81df0c12143ce466d0370db36d37713fae
Raul Donde <glas109@gmail.com>
2023-09-27 00:03:23 -0600

    Fin Gauss

--- P1Actv4/Matrix.java
+++ P1Actv4/Matrix.java
@@ -1,24 +1,72 @@
 import java.util.Scanner;
-public class Matrix {
 
+public class Matrix {
     public static int[][] crearMatrix() {
-        System.out.println("Ingrese una dimensión de la matriz (n * n)");
+        int[][] defaultMatrix = {
+            {2, 1, -1, 8},
+            {-3, -1, 2, -11},
+            {-2, 1, 2, -3}
+        };
+
+        System.out.println("Ingrese una dimensión de la matriz (n * n) [PRESIONE D para DEFAULT MATRIX]");
         Scanner SCANNER = new Scanner(System.in);
-        int dimensiones = Integer.parseInt(SCANNER.nextLine());
+        String a = SCANNER.nextLine();
+        if(a.contains("D")){
+            return defaultMatrix;
+        }
+        int dimensiones = Integer.parseInt(a);
         int[][] matrix = new int[dimensiones][dimensiones];
         // SCANNER.close();
         return matrix;
     }
+
     public static int[][] rellenar(int[][] matrix) {
+        
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.println("ingrese dato para el espacio [" + i + "][" + j + "]");
-                matrix[i][j] = Integer.parseInt(scanner.nextLine());
+                System.out.print("ingrese dato para el espacio [" + i + "][" + j + "]: ");
+                try {
+                    matrix[i][j] = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Error, default a 0");
+                    matrix[i][j] = 0;
+                }
             }
         }
         scanner.close();
         return matrix;
     }
-    
+
+    public static void contarCeros(int[][] matrix) {
+        int ceros = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) ceros++;
+            }
+            System.out.println("# de Ceros en Fila " + i + ": " + ceros);
+            ceros = 0;
+        }
+    }
+
+    public static int[][] aplicarGaussJordan(int[][] matrix){
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            double pivot = matrix[i][i];
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] /= pivot;
+            }
+            for (int k = 0; k < rows; k++) {
+                if (k != i) {
+                    double factor = matrix[k][i];
+                    for (int j = 0; j < cols; j++) {
+                        matrix[k][j] -= factor * matrix[i][j];
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
 }
--- P1Actv4/Program.java
+++ P1Actv4/Program.java
@@ -3,9 +3,18 @@ import java.util.Arrays;
 public class Program {
 
     public static void main(String[] args) {
+        int[][] defaultMatrix = {
+            {2, 1, -1, 8},
+            {-3, -1, 2, -11},
+            {-2, 1, 2, -3}
+        };
+
         int[][] _matrix = Matrix.crearMatrix(); //Obtener un Array de lo escrito por el usuario
-        _matrix = Matrix.rellenar(_matrix);
-        System.out.println(Arrays.deepToString(_matrix));
+        if(!Arrays.deepEquals(defaultMatrix, _matrix))
+            _matrix = Matrix.rellenar(_matrix);
+        System.out.println(Arrays.deepToString(_matrix).replace("],","\n")); //Imprime la matriz
+        Matrix.contarCeros(_matrix);
+        System.out.println(Arrays.deepToString(Matrix.aplicarGaussJordan(_matrix)).replace("],","\n")); //Imprime la matriz
     }
 
 }
