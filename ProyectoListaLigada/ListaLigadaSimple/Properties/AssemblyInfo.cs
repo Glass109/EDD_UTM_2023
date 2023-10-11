50188a57e9bc7a3e233abe80e08fc242151096ec
Raul Donde <glas109@gmail.com>
2023-10-11 00:45:40 -0600

    Actualizado proyecto a .Net 8

---
+++ ProyectoListaLigada/ListaLigadaSimple/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("f4e5536a-4323-4c0f-8736-19b121b18447")]
